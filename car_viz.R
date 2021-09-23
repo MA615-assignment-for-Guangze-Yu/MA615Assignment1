8b918704d07516297d499a5ca87f859c6bc1d6fb
guangze-yu <gzyu@ucdavis.edu>
2021-09-23 18:03:24 -0400

    222

--- car_viz.R
+++ car_viz.R
@@ -6,7 +6,7 @@ data(mtcars)
 
 # Select only car models where mpg<20
 mtcars_mpg2 <- mtcars[mtcars$mpg < 20,]
-## Select for conditional dataframe, which help us to decrease the difficulty to handle of dataset.
+### Select for conditional dataframe, which help us to decrease the difficulty to handle of dataset.
 
 # Reduce the variables to mpg, cyl, disp, hp, gears
 mtcars_mpg2 <- mtcars_mpg2[, c(1,2,3,4,10)]
@@ -32,10 +32,13 @@ ggplot(mtcars_mpg2) +
   geom_point(shape = "bullet", size = 4L, colour = "#B22222") +
   geom_smooth(span = 0.5) +
   theme_minimal()
+
+
 ## Plot for scatter plot of variables.
 
 # note that this boxplot cannot be made with esquisse() unless
 # the data is adjusted.  What adjustment is needed?
+## The cyl should become to factors if want to plot. 
 
 
 ggplot(mtcars_mpg2, aes(x=as.factor(cyl), y=mpg)) + 
@@ -45,6 +48,6 @@ ggplot(mtcars_mpg2, aes(x=as.factor(cyl), y=mpg)) +
 ## Plot for scatter plot of variables by groups.
 
 
-
+## esquisser(data = mtcars_mpg2, viewer = "browser")
 
 
